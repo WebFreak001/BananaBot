0e295a60b2df2f4abdf793346342c6114f85d7ac
WebFreak001 <janju007@web.de>
2015-07-25 00:02:03 +0200

    Made bot more interactive by adding "whats fact #<no>" and !help

--- main.js
+++ main.js
@@ -5,6 +5,7 @@ var fs = require("fs");
 var path = require("path");
 var config = require("./config");
 var facts = [];
+var questionRegex = /(?:tell\s+me|what\s*.?\s*s)\s+Fact\s+(?:#|number|no|n)?\s*(\d+)/i;
 fs.readFile(path.join(__dirname, config.facts), "utf8", function (err, data) {
     if (err)
         throw err;
@@ -17,15 +18,30 @@ fs.readFile(path.join(__dirname, config.facts), "utf8", function (err, data) {
             continue;
         facts.push(line);
     }
-    if (facts.length == 0) {
-        console.warn("No facts found.");
-        return;
+    if (facts.length == 0)
+        return console.warn("No facts found.");
+    var lastMessage = new Date().getTime();
+    var chat = new HackChat.Session(config.channel, "BananaBotDev");
+    function saveSend(message) {
+        if (lastMessage - new Date().getTime() < -2500) {
+            lastMessage = new Date().getTime();
+            chat.sendMessage(message);
+        }
     }
-    var chat = new HackChat.Session(config.channel, "BananaBot");
+    chat.on("chat", function (nick, text) {
+        if (text == "!help" || text == "/help")
+            return saveSend("Either kindly ask me for a fact number or wait 3 minutes to get a free fact. Random facts every 3 minutes!");
+        var result = questionRegex.exec(text);
+        if (result && result.length == 2) {
+            var n = parseInt(result[1]) - 1;
+            if (n >= 0 && n < facts.length)
+                saveSend("Fact #" + (n + 1) + ": " + facts[n]);
+        }
+    });
     chat.on("joining", function () {
         setInterval(function () {
             var fact = Math.floor(Math.random() * facts.length);
-            chat.sendMessage("Fact #" + (fact + 1) + ": " + facts[fact]);
+            saveSend("Fact #" + (fact + 1) + ": " + facts[fact]);
         }, 3 * 60 * 1000);
     });
 });
--- main.ts
+++ main.ts
@@ -8,9 +8,12 @@ var config = require("./config");
 
 var facts = [];
 
+var questionRegex = /(?:tell\s+me|what\s*.?\s*s)\s+Fact\s+(?:#|number|no|n)?\s*(\d+)/i;
+
 // Reads facts out of specified facts file in config.js
 fs.readFile(path.join(__dirname, config.facts), "utf8", function(err, data) {
-    if (err) throw err;
+    if (err)
+        throw err;
 
     var lines = data.split("\n");
 
@@ -24,16 +27,35 @@ fs.readFile(path.join(__dirname, config.facts), "utf8", function(err, data) {
         facts.push(line);
     }
 
-    if (facts.length == 0) {
-        console.warn("No facts found.");
-        return;
+    if (facts.length == 0)
+        return console.warn("No facts found.");
+
+    var lastMessage = new Date().getTime();
+
+    var chat = new HackChat.Session(config.channel, "BananaBotDev"); // join channel and send facts every 3 minutes when joining
+
+    function saveSend(message) {
+        if (lastMessage - new Date().getTime() < -2500) {
+            lastMessage = new Date().getTime();
+            chat.sendMessage(message);
+        }
     }
 
-    var chat = new HackChat.Session(config.channel, "BananaBot"); // join channel and send facts every 3 minutes when joining
+    chat.on("chat", function(nick, text) {
+        if (text == "!help" || text == "/help")
+            return saveSend("Either kindly ask me for a fact number or wait 3 minutes to get a free fact. Random facts every 3 minutes!");
+
+        var result = questionRegex.exec(text);
+        if (result && result.length == 2) {
+            var n = parseInt(result[1]) - 1;
+            if (n >= 0 && n < facts.length)
+                saveSend("Fact #" + (n + 1) + ": " + facts[n]);
+        }
+    });
     chat.on("joining", function() {
         setInterval(function() {
             var fact = Math.floor(Math.random() * facts.length);
-            chat.sendMessage("Fact #" + (fact + 1) + ": " + facts[fact]);
+            saveSend("Fact #" + (fact + 1) + ": " + facts[fact]);
         }, 3 * 60 * 1000); // 3 * 60 * 1000 milliseconds = 3 * 60 seconds = 3 minutes
     });
 });
